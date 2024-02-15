42a2c707b8afbea1cfd824658e967da2f2c65eb4
A3 <mm22-1094@unphu.edu.do>
2024-02-15 19:49:07 -0400

    lo

--- simpleAgenda.java
+++ simpleAgenda.java
@@ -1,7 +1,28 @@
+import java.util.Scanner;
+
 public class simpleAgenda {
-  public static void agregarContacto (String nombre) {
-    System.out.println(nombre + "" );
+  public static void agregarContacto () {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Ingrese un contacto a agregar");
+
+    String NOMBRE = scanner.nextLine();
+
+    if (NOMBRE.length() > 4 && NOMBRE.length() < 8) {
+
+      System.out.println("Contacto agregado con éxito");
+
+    } else if (NOMBRE.length() >= 8 && NOMBRE.length() <= 30) {
+
+      System.out.println("Agregado, contiene entre 8-30 caracteres");
+
+    } else {
+
+      System.out.println("Nombre muy largo");
+    }
   }
+
 
   public static void removerContacto (int id) {
     System.out.println(id + "");
@@ -16,6 +37,8 @@ public class simpleAgenda {
   }
 
   public static void main(String[] args) {
+
+
 
   }
 
