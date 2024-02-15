d9427b3b3e1d891f50533bd45486ca2ebcc6ebcd
A3 <mm22-1094@unphu.edu.do>
2024-02-15 19:24:20 -0400

    lo

--- simpleAgenda.java
+++ simpleAgenda.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class simpleAgenda {
+public class simpleAgenda {
+  public static void agregarContacto (String nombre) {
+    System.out.println(nombre + "" );
+  }
+
+  public static void removerContacto (int id) {
+    System.out.println(id + "");
+  }
+
+  public static String actualizarContacto (int a) {
+
+    return "N/A";
+  }
+
+  public static void mostrarContactos (int b) {
+  }
+
+  public static void main(String[] args) {
+
+  }
+
 }
